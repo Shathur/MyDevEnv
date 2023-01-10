2174a9aa23c1b4137d90cc5f6ca165a7a8cc568e
Shathur <iliaslangas@gmail.com>
2023-01-10 18:15:55 +0200

    setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,11 @@
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# Now from inside vim you need to run the command below
+# :PluginInstall
+
+# To add the tmux functionality do the following:
+# git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+# Reload TMUX env so TPM is sourced
+# tmux source ~/.tmux.conf
+# Now from inside a TMUX session press prefix + I to fetch the plugins
