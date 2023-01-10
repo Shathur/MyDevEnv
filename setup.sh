30f52a61bc014e686ccbcc7d5f52f4f8e700b81b
Shathur <iliaslangas@gmail.com>
2023-01-10 17:55:43 +0200

    second commit

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
- git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp .vimrc ~/.vimrc
+cp .tmux.conf ~/.tmux.conf
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
