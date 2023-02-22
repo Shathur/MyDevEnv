2137076658ef84e9ab45f7b664310a18a6d59ee9
Shathur <iliaslangas@gmail.com>
2023-02-22 14:58:32 +0200

    added essential pip installs

---
+++ python_dev.sh
@@ -0,0 +1,10 @@
+# /bin/bash
+# Necessary installation for python3 development
+pip install --upgrade pip
+pip install pylint
+pip install ipython3
+pip install openai
+pip install jupyter_ascending && \
+python3 -m jupyter nbextension    install jupyter_ascending --sys-prefix --py && \
+python3 -m jupyter nbextension     enable jupyter_ascending --sys-prefix --py && \
+python3 -m jupyter serverextension enable jupyter_ascending --sys-prefix --py
