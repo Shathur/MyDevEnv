d5757d02852f949e925310c72b2b49322a2f5369
Shathur <iliaslangas@gmail.com>
2023-03-02 13:44:43 +0200

    added buildtools

--- system_installations.sh
+++ system_installations.sh
@@ -4,4 +4,5 @@
 sudo apt update &&
 sudo apt upgrade -y &&
 sudo apt-get install libhdf5-dev -y &&
+sudo apt-get install build-essential libssl-dev libffi-dev python-dev -y &&
 sudo apt-get install gcc-aarch64-linux-gnu -y
