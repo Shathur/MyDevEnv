57d85a31b249c58f0fd3f0abadabc8a483aeadbd
Shathur <iliaslangas@gmail.com>
2023-03-02 13:39:33 +0200

    upload

--- system_installations.sh
+++ system_installations.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
 # first install some necessary packages
-sudo apt-get install libhdf5-dev --y &&
-sudo apt-get install gcc-aarch64-linux-gnu
+sudo apt update &&
+sudo apt upgrade -y &&
+sudo apt-get install libhdf5-dev -y &&
+sudo apt-get install gcc-aarch64-linux-gnu -y
